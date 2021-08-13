1fb75338c5922ed13103b1d644f1a1136156665b
pritishposwal <pritish20321@iiitd.ac.in>
2021-08-13 18:11:42 +0530

    trying input

--- Simple-Assembler/assembler.py
+++ Simple-Assembler/assembler.py
@@ -1,4 +1,9 @@
+from sys import stdin
 def main():
-    pass
+    from sys import stdin
+    for line in stdin:
+        if line == '':  # If empty string is read then stop the loop
+            break
+        print(line)  # perform some operation(s) on given string
 if __name__=="__main__":
     main()
