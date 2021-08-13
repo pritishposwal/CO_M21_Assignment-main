6fe8cea747de2b10438077772b41350b48b13808
pritishposwal <pritish20321@iiitd.ac.in>
2021-08-13 19:32:20 +0530

    creating dictonaries

--- Simple-Assembler/assembler.py
+++ Simple-Assembler/assembler.py
@@ -1,6 +1,9 @@
 import os
 def main():
-    # here 0 and 10**6 represents starting point and end point in bytes.
+    opcodes={"add":"00000","sub":"00001","movimm":"00010","movreg":"00011","ld":"00100","st":"00101","mul":"00110","div":"00111",
+             "rs":"01000","ls":"01001","xor":"01010","or":"01011","and":"01100","not":"01101","cmp":"01110","jmp":"01111",
+             "jlt":"10000","jgt":"10001","je":"10010","hlt":"10011"}
+    registers={"R0":"000","R1":"001","R2":"010","R3":"011","R4":"100","R5":"101","R6":"110","FLAGS":"111"}
     lines = os.read(0, 10 ** 6).strip().splitlines()
     file=""
     for x in lines:
